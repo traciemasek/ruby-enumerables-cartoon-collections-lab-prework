c0a53452e59d621c3f08512b27f999c74a1fe137
Tracie Masek <traciemasek@gmail.com>
2019-06-22 21:20:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def summon_captain_planet(calls)
   calls.map { |el| "#{el.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)
   # Your code here
 end
 
