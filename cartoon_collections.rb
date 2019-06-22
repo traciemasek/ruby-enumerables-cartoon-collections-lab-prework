1227c3d9f6d50a7783ae0c9be5b469cd028fc802
Tracie Masek <traciemasek@gmail.com>
2019-06-22 21:19:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index { |el, i| puts "#{i + 1} #{el}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map { |el| #{} } 
 end
 
 def long_planeteer_calls# code an argument here
